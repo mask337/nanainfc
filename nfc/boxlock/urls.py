8d3f3ac2eb7cc872394024bddf178262d3c543f8
ktaka03a <47961915+ktaka03a@users.noreply.github.com>
2019-08-19 23:43:05 +0900

    Revert "created view 'status'"
    This reverts commit af7e358af31b3d742972b30c4d5dc8a4dcdbf3db.

--- nfc/boxlock/urls.py
+++ nfc/boxlock/urls.py
@@ -4,5 +4,4 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('status/<int:user_id>/',views.status,name='status'),
 ]
--- nfc/boxlock/views.py
+++ nfc/boxlock/views.py
@@ -4,7 +4,4 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
-
-def status(request,user_id):
-    return HttpResponse("Here is the list of lockers you have the permission of.")
-
+# Create your views here.
